75d31e6956b7b92ebe5457f8b8c86b63dccdaae2
Pankhuri1804 <39328592+Pankhuri1804@users.noreply.github.com>
2021-07-28 11:14:55 +0530

    Update data.py

--- data.py
+++ data.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 def getdeliveriesdata():
-    return pd.read_csv("IPL/data/deliveries.csv")
+    return pd.read_csv("data/deliveries.csv")
 
 def getmatchesdata():
-    return pd.read_csv("IPL/data/matches.csv")
+    return pd.read_csv("data/matches.csv")
 
